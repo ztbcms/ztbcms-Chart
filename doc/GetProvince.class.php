aa0dca877ba0697f550667babbc55b007c9073aa
jaren <13428282005@163.com>
2017-06-15 17:34:04 +0800

    demo

---
+++ doc/GetProvince.class.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Chart\Script\X;
+
+use Chart\Lib\BaseScriptX;
+
+class GetProvince extends BaseScriptX {
+
+    public function getField() {
+        return 'parentid';
+    }
+
+    public function run() {
+        $x_data = [];
+        $prefix = C('DB_PREFIX');
+        $model = M('area_city a');
+        $x_set = $model
+            ->field('a.id, b.shortname as province_name')
+            ->join('LEFT JOIN ' . $prefix . 'area_province b on a.parentid = b.id')
+            ->group('a.parentid')
+            ->order('id')
+            ->select();
+
+        foreach ($x_set as $item) {
+            $x_data[] = $item['province_name'];
+        }
+
+        $x_data = implode(',', $x_data);
+        return $x_data;
+    }
+}
